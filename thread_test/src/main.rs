4765535e61f73ceca4397ac0a16f3d714aaf82fa
hdd <huangdongdong.com>
2020-10-22 17:04:49 +0800

    test branch_1

--- thread_test/src/main.rs
+++ thread_test/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use rayon::prelude::*;
+use std::{thread,time};
+use std::thread::sleep;
+
+fn main(){
+
+    let handle = thread::spawn(|| {
+        for i in 1..10{
+            println!("hdd spawn1 i = {}", i);
+            sleep(time::Duration::new(1,0));
+        }
+    });
+
+    rayon::scope(|s| {
+        s.spawn(|_| {
+            println!("spawn2 begin");
+            for i in 1..10{
+                println!("hdd spawn2 i = {}", i);
+            }
+        });
+    });
+
+    println!("spawn1 over");
+    handle.join();
+    //sleep(time::Duration::new(1,0));
+    println!("hdd main over");
 }
